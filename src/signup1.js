dc2d2c19466e3cd0d60bb246ef1764e82e6ca5ab
Shreya Langhe <shreyalanghe@gmail.com>
2023-03-09 01:39:24 -0500

    added first signup page html+css

---
+++ src/signup1.js
@@ -0,0 +1,34 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function signup1() {
+
+    
+    return (
+        <body>
+        <div style={{ backgroundColor: "#63b8c7", paddingTop: "0.3%", paddingBottom: "0.3%", paddingLeft: "1%", margin: "0" }}> 
+            <h1>Exploratory</h1>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <center><form>
+            <div className="form-group">
+            <label>Username</label>
+            <input type="text" className="w-25 form-control"  placeholder="Enter username"/>
+            </div>
+            <br />
+            <br />
+            <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="w-25 form-control" id="Password"  placeholder="Password" /> 
+            <small id="passwordHelp" className="form-text text-muted">Your password must be 10 characters long.</small>
+            </div>
+            <br />
+            <button type="button" className="btn btn-outline-primary">Sign Up</button>
+        </form></center>
+    </body>
+    )
+}
